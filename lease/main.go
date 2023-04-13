ddc9b96564115823480959bf4fb14c4ba4e26d27
oguzhanyildirim <oguzhan.yildirim@trendyol.com>
2023-04-13 12:24:29 +0300

    draft: introduce lease pattern

--- lease/main.go
+++ lease/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"time"
+)
 
+func main() {
+	// Create a new lease tracker
+	leases := make(map[string]*Lease)
+	tracker := NewLeaderLeaseTracker(leases)
+
+	// Add a lease to the tracker
+	if err := tracker.AddLease("example_lease", 10*time.Second.Nanoseconds()); err != nil {
+		log.Fatalf("Error adding lease: %v", err)
+	}
+
+	// Start the lease tracker
+	tracker.Start()
+
+	// Wait for some time
+	time.Sleep(20 * time.Second)
+
+	// Stop the lease tracker
+	tracker.Stop()
 }
